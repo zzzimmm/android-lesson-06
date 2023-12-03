7e66ebb1462ab74c75d2f256e3cd22b11247cbeb
zzzimmm <145953647+zzzimmm@users.noreply.github.com>
2023-12-03 18:47:24 +0900

    Update AdminDataRestEndpoint.java
    homework

--- src/main/java/kr/easw/lesson06/controller/AdminDataRestEndpoint.java
+++ src/main/java/kr/easw/lesson06/controller/AdminDataRestEndpoint.java
@@ -20,13 +20,8 @@ public class AdminDataRestEndpoint {
 
     // 이 메서드의 엔드포인트를 /api/v1/data/add로 설정합니다. POST만 허용됩니다.
     @PostMapping("/add")
-    public ModelAndView addText(@RequestParam Map<String, String> data, @RequestBody Map<String, String> body) {
-        // 만약 body로 값이 넘어왔다면 body를, 그렇지 않다면 data를 사용합니다.
-        // 이는 form-data로 넘어왔는지, JSON으로 넘어왔는지에 따라 값이 달라지기 때문입니다.
-        if (data.containsKey("text"))
-            textDataService.addText(new TextDataDto(0L, data.get("text")));
-        else if (body.containsKey("text"))
-            textDataService.addText(new TextDataDto(0L, body.get("text")));
+    public ModelAndView addText(@RequestParam("text") String text) {
+        textDataService.addText(new TextDataDto(0L, text));
         return new ModelAndView("redirect:/admin?success=true");
     }
 
