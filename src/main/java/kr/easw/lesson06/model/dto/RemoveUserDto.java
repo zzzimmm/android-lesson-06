d6b434a061936ba768972f8efc2f8ed2a9fa9caf
zzzimmm <145953647+zzzimmm@users.noreply.github.com>
2023-12-03 18:50:22 +0900

    homework

--- src/main/java/kr/easw/lesson06/model/dto/RemoveUserDto.java
+++ src/main/java/kr/easw/lesson06/model/dto/RemoveUserDto.java
@@ -1,12 +1,16 @@
 package kr.easw.lesson06.model.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
+@Setter
+@NoArgsConstructor
 public class RemoveUserDto {
-    private final String userId;
+    private String userId;
+
+    public RemoveUserDto(String userId) {
+        this.userId = userId;
+    }
 }
