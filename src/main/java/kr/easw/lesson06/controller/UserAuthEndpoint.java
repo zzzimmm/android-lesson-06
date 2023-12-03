80255c2daf62cf01dc42851b64ef150a62a82e31
zzzimmm <145953647+zzzimmm@users.noreply.github.com>
2023-12-03 18:47:55 +0900

    Update UserAuthEndpoint.java
    homework

--- src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
+++ src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
@@ -6,16 +6,20 @@ import kr.easw.lesson06.model.dto.UserDataEntity;
 import kr.easw.lesson06.service.UserDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class UserAuthEndpoint {
     private final UserDataService userDataService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
     // JWT 인증을 위해 사용되는 엔드포인트입니다.
@@ -32,10 +36,26 @@ public class UserAuthEndpoint {
 
 
     @PostMapping("/register")
-    public void register(@ModelAttribute UserDataEntity entity) {
-        // 유저 회원가입을 구현하십시오.
-        // 해당 메서드를 작성하기 위해서는, UserDataService와 admin_dashboard.html을 참고하십시오.
-        // 해당 메서드는 register.html에서 사용됩니다.
-        throw new IllegalStateException("Not implemented yet");
+    public ResponseEntity<Object> register(HttpServletRequest request) {
+        try {
+            String userId = request.getParameter("username");
+            String rawPassword = request.getParameter("password");
+
+            // 사용자의 존재 여부를 확인합니다.
+            if (userDataService.isUserExists(userId)) {
+                return ResponseEntity.badRequest().body("User already exists");
+            }
+
+            // 비밀번호를 암호화합니다.
+            String encryptedPassword = bCryptPasswordEncoder.encode(rawPassword);
+
+            // 새로운 사용자 객체를 생성하고 비밀번호를 설정합니다.
+            UserDataEntity newUser = new UserDataEntity(0L, userId, encryptedPassword, false);
+            userDataService.createUser(newUser);
+
+            return ResponseEntity.ok("User successfully registered");
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body("Registration failed: " + ex.getMessage());
+        }
     }
 }
