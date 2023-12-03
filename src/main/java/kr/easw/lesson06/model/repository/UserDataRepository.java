007fbfc0fda104d98a18adfe63e073b1a4991c5a
zzzimmm <145953647+zzzimmm@users.noreply.github.com>
2023-12-03 18:51:30 +0900

    homework

--- src/main/java/kr/easw/lesson06/model/repository/UserDataRepository.java
+++ src/main/java/kr/easw/lesson06/model/repository/UserDataRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserDataRepository extends JpaRepository<UserDataEntity, Long> {
     Optional<UserDataEntity> findUserDataEntityByUserId(String userId);
+    Optional<UserDataEntity> findByUserId(String userId);
 }
